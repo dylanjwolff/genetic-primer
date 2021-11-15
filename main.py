ff9d10fefa064ac6d53a14e5939dd32942fb3d59
dylanjwolff <wolffdy0@gmail.com>
2021-11-15 14:17:29 +0800

    cleanup

--- main.py
+++ main.py
@@ -1,20 +1,31 @@
+import time
 from geneticalgorithm import geneticalgorithm as ga
 import editdistance
 import numpy as np
+import sys
 
 BEST = []
 DIM = 20
 THRESH = DIM*0.4
 CG_MIN = 0.4 * DIM
 CG_MAX = 0.55 * DIM
+SKIP_COUNT = 0
+EXEC_COUNT = 0
+PRIMERS = 1000
 
 
 def feedback(candidate):
+    global EXEC_COUNT
+    global SKIP_COUNT
+    global BEST
+    EXEC_COUNT += 1
+
     cg_count = 0
     for base in candidate:
         if base == 1 or base == 2:
             cg_count += 1
     if cg_count < CG_MIN or CG_MAX < cg_count:
+        SKIP_COUNT += 1
         return 0
 
     if len(BEST) == 0:
@@ -32,12 +43,57 @@ def feedback(candidate):
     cost = min(distances) - (1/total)
     return -cost
 
+def check_cg_ok(candidate):
+    global SKIP_COUNT
+    cg_count = 0
+    for base in candidate:
+        if base == 1 or base == 2:
+            cg_count += 1
+    if cg_count < CG_MIN or CG_MAX < cg_count:
+        SKIP_COUNT += 1
+        return False
+
+    return True
+
+
+def squish_feedback(candidate):
+    separated = np.reshape(candidate, (int(len(candidate)/DIM), DIM))
+
+    distance = 0
+    for row_num, row in enumerate(separated):
+        if not check_cg_ok(row):
+            return np.finfo('d').max
+
+        for other_row in separated[row_num+1:]:
+            this_distance = editdistance.eval(row, other_row)
+            if this_distance < THRESH:
+                return np.finfo('d').max
+            distance += this_distance
+
+    return distance
 
 varbound = np.array([[1, 4]]*DIM)
 
-model = ga(function=feedback, dimension=DIM, variable_type='int',
+search_model = ga(function=feedback, dimension=DIM, variable_type='int',
            variable_boundaries=varbound)
 
-while len(BEST) < 2000:
-    model.run()
-    print(len(BEST))
+varbound = np.array([[1, 4]]*DIM*len(BEST))
+squish_model = ga(function=squish_feedback, dimension=DIM*len(BEST), variable_type='int',
+           variable_boundaries=varbound)
+
+# print(squish_model.pop_s)
+while len(BEST) < PRIMERS:
+# while False:
+    start = time.time()
+    search_model.run()
+    end = time.time()
+    print(f"{BEST[-1]} candidate")
+    print(f"{len(BEST)} candidates found")
+    print(f"{end - start} elapsed")
+    print(f"{SKIP_COUNT} of {EXEC_COUNT} skipped")
+    EXEC_COUNT = 0
+    SKIP_COUNT = 0
+
+# ina = np.array([0.]*40)
+# r = squish_feedback(ina)
+# print(r)
