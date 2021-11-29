984d3fd86f65e8e9c98e68031c419faf1307fda1
wolffd <wolffdy0@gmail.com>
2021-11-29 09:20:22 +0800

    adding quick graph generation

--- analysis.py
+++ analysis.py
@@ -2,10 +2,22 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 
-df = pd.read_csv("log.csv", names=[
-                 "time", "primers", "mean distance", "mean number of tangent primers"])
-df["time"] = df["time"] - df["time"][0]
+# filenames = ["data/20L-9ED-genetic.csv", "data/25L-9ED-genetic.csv", "data/30L-9ED-genetic.csv"]
+filenames = ["data/20L-8ED-genetic.csv", "data/20L-9ED-genetic.csv", "data/20L-10ED-genetic.csv"]
+# filenames = ["data/20L-9ED-genetic.csv", "data/20L-9ED-one-by-one.csv"]
+
+dfs = []
+for filename in filenames:
+    df = pd.read_csv(filename, names=[
+                     "time", "primers", "mean distance", "mean number of tangent primers"])
+    df["time"] = df["time"] - df["time"][0]
+    dfs.append(df)
+df = pd.concat(dfs,
+                keys=filenames,
+                names=["file", "data point"],
+                join="inner")
+df = df.reset_index()
 print(df)
 
-sns.lineplot(x="time", y="primers", data=df)
+sns.lineplot(x="time", y="primers", data=df, hue="file")
 plt.show()
